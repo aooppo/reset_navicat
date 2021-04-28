3a38a52cf917f422935f0bbdf3aaf2c1e55eb892
ctj <i@voox.cc>
2021-04-28 10:02:25 +0800

    Create reset_navicat.bat

---
+++ reset_navicat.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCS
+echo waiting......
+reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCS" /va /f
+echo.
+
+echo Delete Info folder under HKEY_CURRENT_USER\Software\Classes\CLSID
+echo waiting......
+
+for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
+    reg delete %%i /va /f
+)
+
+echo.
+echo Finish
+
+pause
